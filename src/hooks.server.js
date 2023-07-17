e61ac16373ab644d9e3a78cfd88f7e1c01d5df59
andrea <andrea.lacamera0@gmail.com>
2023-07-17 16:29:07 +0200

    bug solved

--- src/hooks.server.js
+++
@@ -1,7 +0,0 @@
-// import type { RequestEvent, Handle } from '@sveltejs/kit';
-export const handle = (({ event, resolve }) => {
-  console.log(event)
-  return resolve(event, {
-    transformPageChunk: ({ html }) => html.replace('%lang%', "en")
-  });
-});// satisfies Handle;
--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,12 +1,12 @@
 import { loadTranslations } from '$lib/i18n';
 
 /** @type {import('@sveltejs/kit').Load} */
-export const load = async ({ url }) => {
-  const { pathname } = url;
-
+export const load = async () => {
   const initLocale = 'en'; // get from cookie, user session, ...
 
-  await loadTranslations(initLocale, pathname); // keep this just before the `return`
+  await loadTranslations(initLocale, '/') // keep this just before the `return`
 
   return {};
 }
+
+export const prerender = true;
